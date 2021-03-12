2d65935be56988be3181b4d74a1c1886ec689b5a
Andre Luis Carvalho Alves <andre.alves@solutis.net.br>
2021-03-11 22:51:01 -0300

    teste do producer, incluindo 10 msg para envio para o topico

--- simpleProducer/Program.cs
+++ simpleProducer/Program.cs
@@ -1,14 +1,45 @@
 ﻿using System;
+using Serilog;
+using System.Net;
 using Confluent.Kafka;
-using Confluent.Kafka.Serialization;
 
 namespace simpleProducer
 {
     class Program
     {
+        const string KafkaServer = "127.0.0.1:9092";
+        const string KafkaTopic  = "testTopic";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .CreateLogger();
+            logger.Information("Testando o envio de mensagens com Kafka");
+
+            if (args.Length < 10)
+            {
+                logger.Error(
+                    "Informe ao menos 10 parâmetros: " +
+                    "contendo as mensagens a serem enviadas para fila" +
+                    "que serão enviadas a um Topic no Kafka...");
+                return;
+            }
+
+            var producerConfig = new ProducerConfig{
+                BootstrapServers = KafkaServer,
+                ClientId = Dns.GetHostName()
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
+            {
+                // Send 10 messages to the topic
+                for (int i = 0; i < 10; i++)
+                {
+                    var result = producer.ProduceAsync(KafkaTopic,  new Message<Null,string> { Value = args[i] }).GetAwaiter().GetResult();
+                    Console.WriteLine($"Event {i} sent on Partition: {result.Partition} with Offset: {result.Offset}");
+                }
+            }
         }
     }
 }
